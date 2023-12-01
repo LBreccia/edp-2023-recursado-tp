4fcb968d14a4eaeea15f214f925b8e5329f79538
florencia-j <fcjusto11@gmail.com>
2023-12-01 15:31:30 -0300

    Cambios en menu, agregando monitor.sh, analizar.sh, clasificar.sh

--- 02-usuario/menu.sh
+++ 02-usuario/menu.sh
@@ -2,22 +2,19 @@
 
 PS3='Elija> '
 
-function clasificar {
-       echo LALALA
-}       
 
 echo "Hola, soy el menú del TP. ¿Qué quiere hacer?"
 select opcion in "Clasificar arhivos" "Monitorear sistema" "Analizar textos" "Salir"
 do
 	[ -z "$opcion" ] && echo "elegir opción válida." && continue
-	[ $REPLY == 1 ] &&  clasificar  && continue
-	[ $REPLY == 2 ] && echo "Eligió monitorear sistema" && continue
-	[ $REPLY == 3 ] && echo "Analizar textos" && continue
+	[ $REPLY == 1 ] && bash clasificar.sh  && continue
+	[ $REPLY == 2 ] && bash monitor.sh  && continue
+	[ $REPLY == 3 ] && bash analizar.sh && continue
 	[ $REPLY == 4 ] && echo "Eligió salir" && break
 	echo "Opción elegida: "  $opcion
 done
 
 exit 0
 
-	
+
 
